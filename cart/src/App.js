d6142bb742dc4a9097fcdab95f113f408702be06
gandharv dalal <gandharvdalal123@gmail.com>
2023-02-06 14:11:08 +0530

    Added Navbar

--- cart/src/App.js
+++ cart/src/App.js
@@ -1,14 +1,15 @@
 
-
+import React from "react";
 import Cart from "./Cart";
+import Navbar from "./Navbar";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-    
+      
+        <Navbar/>
         <Cart/>
-      </header>
+      
     </div>
   );
 }
